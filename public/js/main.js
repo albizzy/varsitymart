c55c5aa9ff2d91ab19afdacbde9b196c15331a79
Albert Mwasisoba <almwassy@gmail.com>
2021-08-30 12:31:09 +0300

    made more changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+// Opens up the navigation in small devices
 const addPhoneNav = document.querySelector('.add-phone-nav');
 
 const openPhoneNav = document.querySelector('.open-nav');
@@ -10,4 +11,17 @@ openPhoneNav.addEventListener('click', () => {
 
 closePhoneNav.addEventListener('click', () => {
     addPhoneNav.classList.remove('phone-nav-show');
+})
+
+const addLoginModal = document.querySelector('.add-login-modal');
+const openLoginModal = document.querySelector('.open-login-modal');
+
+openLoginModal.addEventListener('click', () => {
+    addLoginModal.classList.add('login-modal-show');
+})
+
+window.addEventListener('click', e => {
+    if (e.target === addLoginModal) {
+        addLoginModal.classList.remove('login-modal-show')
+    }
 })
